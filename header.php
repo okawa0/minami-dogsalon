5efe239ed6e04045f2017fe40773bdcb9276731f
Makoto Okawa <167591230+okawa0@users.noreply.github.com>
2025-12-05 15:30:29 +0900

    2025.12.5 ヘッダーhtml追加 大川

--- header.php
+++ header.php
@@ -7,10 +7,41 @@
   <title>Document</title>
   <meta name="description" content="説明文">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/top-page.css">
 
 </head>
 
 <body>
-  <header>
+  <header class="site-header">
+    <div class="header__container">
+      <div class="header__logo">
+        <a class="header__logo-link" href="/">
+          <img class="header__logo-img" src="<?php echo get_template_directory_uri(); ?>/img/logo-minami-dog-salon.png" alt="ロゴ">
+          <span class="header__logo-title">みなみドッグサロン</span>
+        </a>
+      </div>
+
+      <button class="header__menu-btn" type="button" aria-expanded="false" aria-controls="global-nav" aria-label="メニューを開く">
+        <span class="header__menu-line"></span>
+        <span class="header__menu-line"></span>
+        <span class="header__menu-line"></span>
+      </button>
+
+      <nav class="header__nav" id="global-nav" aria-label="グローバルナビゲーション">
+        <ul class="nav-list">
+          <li class="nav-list__item"><a href="#">ホーム</a></li>
+          <li class="nav-list__item"><a href="#">オンライン予約</a></li>
+          <li class="nav-list__item">
+            <ul class="nav-list__item-sns">
+              <li class="nav-list__item-sns-icon"><a class="nav-list__item-sns-icon-link" href="#"><img class="nav-list__item-sns-icon-img" src="<?php echo get_template_directory_uri(); ?>/img/facebook-logo.png" alt="facebookロゴ"></a></li>
+              <li class="nav-list__item-sns-icon"><a class="nav-list__item-sns-icon-link" href="#"><img class="nav-list__item-sns-icon-img" src="<?php echo get_template_directory_uri(); ?>/img/instagram-logo.png" alt="instagramロゴ"></a></li>
+              <li class="nav-list__item-sns-icon"><a class="nav-list__item-sns-icon-link" href="#"><img class="nav-list__item-sns-icon-img" src="<?php echo get_template_directory_uri(); ?>/img/yelp-logo.png" alt="yelpロゴ"></a></li>
+            </ul>
+          </li>
+          <li class="nav-list__item"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/user-icon.png" alt="user-icon">ログイン</a></li>
+          <li class="nav-list__item"><a class="nav-list__item-cta" href="#">今すぐ予約</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
