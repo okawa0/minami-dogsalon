d422ba4a830b06a5567d9a8badf6b1cfd474e088
Makoto Okawa <167591230+okawa0@users.noreply.github.com>
2025-12-15 13:09:17 +0900

    フッターPC完成

--- footer.php
+++ footer.php
@@ -4,6 +4,12 @@
     <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram-logo.png" alt="instagramロゴ"></a>
     <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/yelp-logo.png" alt="yelpロゴ"></a>
   </div>
+  <div class="footer__menu">
+    <a class="footer__menu-link" href="#">プライバシーポリシー</a>
+    <a class="footer__menu-link" href="#">Cookie（クッキー） ポリシー</a>
+    <a class="footer__menu-link" href="#">特定商取引法に基づく表記（例）</a>
+  </div>
+  <p class="footer__copyright">© 2024 みなみドッグサロン</p>
 </footer>
 <?php wp_footer();
 ?>
